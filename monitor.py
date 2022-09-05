5cb550335a6eecc295ae9de6f8598a78668d6302
facundoic <facundoceballos1994@hotmail.com>
2022-09-04 21:20:41 -0300

    change in monitor.py

--- monitor.py
+++ monitor.py
@@ -1,9 +1,11 @@
+import re
+
 file = open('/var/lib/docker/containers/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f-json.log', 'r')
 
 def main():
     for line in file:
-        line_split = line.split(':')
-        print(line_split[1])
+        line_split = line.split(':')[1]
+        print(line_split)
 
     file.close()
 
