683897a54cffe3389c33e04b5fcd524dfc683ede
facundoic <facundoceballos1994@hotmail.com>
2022-09-04 21:22:02 -0300

    change in monitor.py

--- monitor.py
+++ monitor.py
@@ -1,11 +1,13 @@
 import re
 
 file = open('/var/lib/docker/containers/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f-json.log', 'r')
+pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
 def main():
     for line in file:
         line_split = line.split(':')[1]
-        print(line_split)
+        ip = re.findall(pattern, line_split)
+        print(ip)
 
     file.close()
 
