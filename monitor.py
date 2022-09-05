62a3c72375629723c60108547dd7ae4c3dc35056
facundoic <facundoceballos1994@hotmail.com>
2022-09-04 21:32:57 -0300

    change in monitor.py

--- monitor.py
+++ monitor.py
@@ -2,14 +2,17 @@ import re
 
 file = open('/var/lib/docker/containers/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f-json.log', 'r')
 pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+result = open("/home/ec2-user/Proyect/Logs/ip.txt", 'w')
 
 def main():
     for line in file:
         line_split = line.split(':')[1]
         ip = re.findall(pattern, line_split)
-        print(ip)
+        
+        result.write(ip)
 
     file.close()
+    result.close()
 
 if __name__ == '__main__':
     main()
