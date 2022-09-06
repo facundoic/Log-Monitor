7d58e1ec61219d332b01e65c3ec5dd9617f499bb
Facundo Ceballos <63003518+facundoic@users.noreply.github.com>
2022-09-06 20:49:44 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,7 +4,7 @@ import os
 
 file = open('/var/lib/docker/containers/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f-json.log', 'r')
 pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-result = open("/home/ec2-user/Proyect/Logs/ip.txt", 'a')
+result = open("/home/ec2-user/Proyect/Logs/ip.txt", 'w')
 
 
 def main():
@@ -26,7 +26,7 @@ if __name__ == '__main__':
                 time.sleep(20)
             else: 
                 os.remove(result)
-                result = open("/home/ec2-user/Proyect/Logs/ip.txt", 'a')
+                result = open("/home/ec2-user/Proyect/Logs/ip.txt", 'w')
                 continue
         except:
             print("An error has been occurred!")
