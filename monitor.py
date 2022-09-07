9b386cc2865911cfaee80ffdcdfd32bb2f222091
Facundo Ceballos <63003518+facundoic@users.noreply.github.com>
2022-09-06 22:15:12 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,25 +4,26 @@ import os
 
 file = open('/var/lib/docker/containers/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f-json.log', 'r')
 pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-result = open("/home/ec2-user/Project/Logs/ip.txt", 'r+')
+
 
 
 def main():
     global result
     while True:
-        for line in file:
-            if len(result.readlines()) < 20:
-                print("I\'am Working!")
-                print(result.readlines())
-                line_split = line.split(':')[1]
-                ip = re.findall(pattern, line_split)
+        with open("/home/ec2-user/Project/Logs/ip.txt", 'r+') as result:
+            for line in file:
+                if len(result.readlines()) < 20:
+                    print("I\'am Working!")
+                    print(result.readlines())
+                    line_split = line.split(':')[1]
+                    ip = re.findall(pattern, line_split)
 
-                if len(ip) != 0:
-                    result.write(str(ip[0]))
-            else:
-                print("The file is complete!")
-                file.close()
-                result.close()
-                continue        
+                    if len(ip) != 0:
+                        result.write(str(ip[0]))
+                else:
+                    print("The file is complete!")
+                    file.close()
+                    result.close()
+                    continue        
 if __name__ == '__main__':
    main()
