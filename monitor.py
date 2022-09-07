3d81fa99d78d769d3f47efbee7eb0b326c34ba32
Facundo Ceballos <63003518+facundoic@users.noreply.github.com>
2022-09-06 22:56:51 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -5,7 +5,7 @@ import os
 file = open('/var/lib/docker/containers/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f-json.log', 'r')
 pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
-with open("/home/ec2-user/Project/Logs/ip.txt", 'w+') as result:
+with open("/home/ec2-user/Project/Logs/ip.txt", 'r+') as result:
     while True:
         for line in file:
             if len(result.readlines()) < 20:
