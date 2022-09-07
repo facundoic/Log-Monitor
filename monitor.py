4668e33a2f7d789c7fa8760b35ff713ea04f1cae
Facundo Ceballos <63003518+facundoic@users.noreply.github.com>
2022-09-06 22:17:05 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -10,7 +10,7 @@ pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 def main():
     global result
     while True:
-        with open("/home/ec2-user/Project/Logs/ip.txt", 'r+') as result:
+        with open("/home/ec2-user/Project/Logs/ip.txt", 'w+') as result:
             for line in file:
                 if len(result.readlines()) < 20:
                     print("I\'am Working!")
