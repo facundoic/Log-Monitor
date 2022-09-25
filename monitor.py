77c17905599841d257ef9459730a312440670fb6
facundoic <facundoceballos1994@hotmail.com>
2022-09-25 16:48:07 -0300

    We add environment variables

--- monitor.py
+++ monitor.py
@@ -1,9 +1,12 @@
 import re
 import time
 import os
+from  dotenv import load_dotenv
 
+load_dotenv('./.env')
 # file = open('/var/lib/docker/containers/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f/b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f-json.log', 'r')
-file = open('./b124597d22dd3508029ee5a394244ff9e1899a510d99ae65f09d9ced5b20034f-json.log', 'r')
+
+file = open(os.getenv('LOG-FILE'), 'r')
 pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
 
